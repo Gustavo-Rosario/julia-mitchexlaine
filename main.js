839ed31fdc8d4910b70f0bd1701d1910abcdf121
Bianca <biancacquintino@gmail.com>
2019-05-15 10:50:25 -0300

    alterei a bia

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 (function(){
   alert("Teste show da julia");
+
+  function bia(){
+    alert("Eu sou a bia e amo o D2");
+   }
 }
   // não sei o que fazer vou colocar esse comentário
   // nada vem a minha mente
- function bia(){
-  alert("Eu sou a bia e amo o D2");
- }
+ 
 
 )();
